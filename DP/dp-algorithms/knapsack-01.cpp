9ac92f635e3cf6b1e31550a8340f058b1306b087
Neel <npaul_be19@thapar.edu>
2022-02-15 23:07:29 +0530

    updated

--- DP/dp-algorithms/knapsack-01.cpp
+++ DP/dp-algorithms/knapsack-01.cpp
@@ -34,13 +34,42 @@ int tabulation(int max_wt){
 	display(dp);
 	return dp[n-1][max_wt];
 }
-
+int spaceOptimization(int max_wt){
+	int n=a.size();
+	vector<int>prev(max_wt+1,0),current(max_wt+1,0);
+	auto [w0,p0]=a[0];
+	for(int rem_wt=w0;rem_wt<=max_wt;++rem_wt)prev[rem_wt]=p0;
+	for(int i=1;i<n;++i){
+		for(int rem_wt=0;rem_wt<=max_wt;++rem_wt){
+			auto [wi,pi]=a[i];
+			int exc=prev[rem_wt],inc=(wi<=rem_wt)?pi+prev[rem_wt-wi]:INT_MIN;
+			current[rem_wt]=max(inc,exc);
+		}prev=current;
+	}
+	return current[max_wt];
+}
+int singleArraySpaceOptimization(int max_wt){
+	int n=a.size();
+	vector<int>dp(max_wt+1,0);
+	auto [w0,p0]=a[0];
+	for(int w=w0;w<=max_wt;++w)dp[w]=p0;
+	for(int i=1;i<n;++i){
+		for(int w=max_wt;w>=0;--w){
+			auto [wi,pi]=a[i];
+			int exc=dp[w],inc=(wi<=w)?pi+dp[w-wi]:INT_MIN;
+			dp[w]=max(inc,exc);
+		}
+	}
+	return dp[max_wt];
+}
 int main(){
 	std::vector<pair<int,int>>v={{3,30},{2,40},{5,60}};
 	a=v;
 	int n=a.size(),w=6;
 	// int ans=solve(n-1,w);
-	int ans=tabulation(w);
+	// int ans=tabulation(w);
+	// int ans=spaceOptimization(w);
+	int ans=singleArraySpaceOptimization(w);
 	cout<<ans<<endl;
 	return 0;
 }
